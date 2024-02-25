0a78c33cbc64d28708a786f282ef7851eace3229
00243pinhas <pinaslok@gmail.com>
2024-02-26 01:14:02 +0300

    image fixages

--- main.js
+++ main.js
@@ -29,44 +29,46 @@ import './scss/index.scss'
 
 
 
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".links-container");
-const links = document.querySelector(".links");
+// const navToggle = document.querySelector(".nav-toggle");
+// const linksContainer = document.querySelector(".links-container");
+// const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-  // linksContainer.classList.toggle("show-links");
+// navToggle.addEventListener("click", function () {
+//   // linksContainer.classList.toggle("show-links");
 
-  const linksHeight = links.getBoundingClientRect().height;
-  const containerHeight = linksContainer.getBoundingClientRect().height;
-  if (containerHeight === 0) {
-    linksContainer.style.height = `${linksHeight}px`;
-  } else {
-    linksContainer.style.height = 0;
-  }
-  // console.log(linksContainer.getBoundingClientRect());
-});
+//   const linksHeight = links.getBoundingClientRect().height;
+//   const containerHeight = linksContainer.getBoundingClientRect().height;
+//   if (containerHeight === 0) {
+//     linksContainer.style.height = `${linksHeight}px`;
+//   } else {
+//     linksContainer.style.height = 0;
+//   }
+//   // console.log(linksContainer.getBoundingClientRect());
+// });
 
 // ********** fixed navbar ************
 
-const navbar = document.getElementById("nav");
-const topLink = document.querySelector(".top-link");
+// const navbar = document.getElementById("nav");
+// const topLink = document.querySelector(".top-link");
 
-window.addEventListener("scroll", function () {
-    const scrollHeight = window.pageYOffset;
-    const navHeight = navbar.getBoundingClientRect().height;
-    if (scrollHeight > navHeight) {
-      navbar.classList.add("fixed-nav");
-    } else {
-      navbar.classList.remove("fixed-nav");
-    }
+// window.addEventListener("scroll", function () {
+//     const scrollHeight = window.pageYOffset;
+//     const navHeight = navbar.getBoundingClientRect().height;
+//     if (scrollHeight > navHeight) {
+//       navbar.classList.add("fixed-nav");
+//     } else {
+//       navbar.classList.remove("fixed-nav");
+//     }
     // setup back to top link
   
-    if (scrollHeight > 500) {
-      console.log("helo");
+    // if (scrollHeight > 300) {
+    //   console.log("helo");
   
-      topLink.classList.add("show-link");
-    } else {
+    //   topLink.classList.add("show-link");
+    // } else {
         
-      topLink.classList.remove("show-link");
-    }
-  });
+    //   // topLink.classList.remove("show-link");
+    // }
+  // });
+
+
