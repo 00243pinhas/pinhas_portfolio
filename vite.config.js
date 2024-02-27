a8cafbd08083e514850ae0b328ba773fcccac30a
00243pinhas <pinaslok@gmail.com>
2024-02-27 20:59:37 +0300

    config two

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-
+    // Specify the output directory for the built files
     outDir: 'dist',
-    
+    // Set assetsDir to '.' to indicate that assets are in the root directory
     assetsDir: '.',
-    
-    assetsInlineLimit: 0, 
+    // Set cssDir to the path of your SCSS folder relative to the root directory
+    css: {
+      preprocessorOptions: {
+        // Set the directory where your SCSS files are located
+        scss: {
+          // Specify the path of your SCSS folder relative to the root directory
+          additionalData: `@import "./scss/index.scss";`,
+        },
+      },
+    },
+    // Specify any additional asset types you want to copy directly to the output directory
+    assetsInlineLimit: 0, // Set to 0 to disable asset inlining
   },
-
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: 'es2018',
-  }
-  
+  // Specify any additional Vite plugins or configurations here
 });
