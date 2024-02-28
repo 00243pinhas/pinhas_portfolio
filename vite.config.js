670a7fba931fb435f0d63dfd7b4e03c3525ea70f
00243pinhas <pinaslok@gmail.com>
2024-02-28 16:09:01 +0300

    config 5

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    
-    outDir: 'dist',
-   
-    assetsDir: './aseets',
-    
-    css: {
-      preprocessorOptions: {
-    
-        scss: {
-          additionalData: `@import "./scss/index.scss";`,
-        },
+    outDir: 'dist', // Specify the output directory for the built files (in the root)
+    rollupOptions: {
+      input: {
+        main: './main.js', // Set the entry point for Rollup (assuming script.js is in the root)
       },
     },
-   
-    assetsInlineLimit: 0, 
+    minify: true, // Enable minification
+    target: 'es2018', // Specify the target environment
   },
- 
 });
