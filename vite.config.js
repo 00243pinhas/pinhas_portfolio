e398226d8aa7b4704a633496b995b695ed4a5ff4
00243pinhas <pinaslok@gmail.com>
2024-02-29 13:48:50 +0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,29 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
- 
+  // Set the root directory to the project root
   root: './',
-
+  // Configure the output directory for the built files
   build: {
     outDir: 'dist',
-
+    // Configure Rollup options
     rollupOptions: {
-
       output: {
-      
+        entryFileNames: '[name].[hash].js', // Customize the output file names (optional)
+        chunkFileNames: '[name].[hash].js', // Customize the chunk file names (optional)
+        assetFileNames: '[name].[hash].[ext]', // Customize the asset file names (optional)
+        // Output JavaScript files directly in the dist directory
         dir: 'dist',
       },
     },
   },
-
+  // Configure the entry points for JavaScript and SCSS files
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
+
+  minify: false, // Enable minification
+  target: 'es2018', // Specify the target environment
 });
