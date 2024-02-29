ed75d2be402faa17e0fad2a0016efd19603a0dd5
00243pinhas <pinaslok@gmail.com>
2024-02-29 14:21:57 +0300

    asstes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
+
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.', // Set the root directory to the project root
+  // Set the root directory to the project root
+  root: './',
+  // Configure the output directory for the built files
   build: {
-    outDir: 'dist', // Specify the output directory for the built files (in the root)
-    assetsDir: '.', // Set the assets directory to the project root
+    outDir: 'dist',
+    // Configure Rollup options
     rollupOptions: {
-      input: {
-        main: './src/main.js', // Set the entry point for Rollup to the main.js file in the src directory
+      // Specify the output directory for JavaScript files
+      rollupOptions: {
+        input: {
+          main: './src/main.js', // Set the entry point for Rollup to the main.js file in the src directory
+        },
+    
       },
+    }
+  },
+  
+  // Configure the entry points for JavaScript and SCSS files
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-      },
-    },
-    minify: false, // Enable minification
-    target: 'es2018', // Specify the target environment
   },
 });
