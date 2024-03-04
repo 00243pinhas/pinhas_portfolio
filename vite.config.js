bcc83b9cdcbe792b6de302514a437eef285beb47
00243pinhas <pinaslok@gmail.com>
2024-03-05 00:54:01 +0300

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
   base: './',
   build: {
-    outDir: 'dist',
+    outDir: './dist',
     
     rollupOptions: {
   
